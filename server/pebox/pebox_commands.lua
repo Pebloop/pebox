caa2ef2f44004323058725b3e610bc5395dba4b5
pebloop <pebloop@outlook.com>
2024-07-21 09:11:07 +0200

    fix error

--- server/pebox/pebox_commands.lua
+++ server/pebox/pebox_commands.lua
@@ -2,6 +2,8 @@ local PeboxCommands = {}
 
 local PeboxCore = require("pebox.pebox_core")
 
+local modem = peripheral.find( "modem" )
+
 function PeboxCommands.acceptCode(deviceID, playerID)
     modem.transmit( 6, 5, deviceID .. " CODE_ACCEPTED " .. playerID)
 end
