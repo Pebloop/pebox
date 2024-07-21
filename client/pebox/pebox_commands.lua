6cdddba8473fda2035647c60e0f72435c96948f8
pebloop <pebloop@outlook.com>
2024-07-21 09:17:14 +0200

    fix error

--- client/pebox/pebox_commands.lua
+++ client/pebox/pebox_commands.lua
@@ -3,6 +3,7 @@ local PeboxCommands = {}
 local PeboxCore = require("pebox.pebox_core")
 
 local id = os.getComputerID()
+local modem = peripheral.find( "modem" )
 
 function PeboxCommands.codeStart(code, pseudo)
     modem.transmit( 6, 5, id .. " CODE_START " .. code .. " " .. pseudo)
