72403c5c5846f4371ad6e7de1d16ad59d34ce268
pebloop <pebloop@outlook.com>
2024-07-21 07:55:37 +0200

    Update code

--- server/pebox/pebox_commands.lua
+++ server/pebox/pebox_commands.lua
@@ -1,6 +1,6 @@
 local PeboxCommands = {}
 
-local PeboxCore = require(shell.dir() .. "/pebox_core")
+local PeboxCore = require(shell.dir() .. ".pebox_core")
 
 function PeboxCommands.acceptCode(deviceID, playerID)
     modem.transmit( 6, 5, deviceID .. " CODE_ACCEPTED " .. playerID)
