16c2f55c0f6827e057a8ffe74d2c53218a41c497
pebloop <pebloop@outlook.com>
2024-07-21 09:29:43 +0200

    fix wrong channels

--- client/pebox/pebox_commands.lua
+++ client/pebox/pebox_commands.lua
@@ -6,11 +6,11 @@ local id = os.getComputerID()
 local modem = peripheral.find( "modem" )
 
 function PeboxCommands.codeStart(code, pseudo)
-    modem.transmit( 6, 5, id .. " CODE_START " .. code .. " " .. pseudo)
+    modem.transmit( 5, 6, id .. " CODE_START " .. code .. " " .. pseudo)
 end
 
 function PeboxCommands.gameStart(playerID)
-    modem.transmit( 6, 5, playerID .. " GAME_START")
+    modem.transmit( 5, 6, playerID .. " GAME_START")
 end
 
 return PeboxCommands
