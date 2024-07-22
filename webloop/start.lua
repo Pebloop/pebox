ebf2b607d0366e401156cb546028644ac04d49ce
pebloop <pebloop@outlook.com>
2024-07-22 21:19:33 +0200

    Add stuff

--- webloop/start.lua
+++ webloop/start.lua
@@ -1,6 +1,8 @@
 local args = {...}
 local filePath = args[1]
-local  env = {}
+local  env = {
+    print = print
+}
 
 local file = fs.open(shell.dir() .. "/" .. filePath, "r")
 local code = loadstring(file.readAll())
