96a609964493c5e51e871c301b70963d55599bb4
pebloop <pebloop@outlook.com>
2024-07-22 21:11:02 +0200

    Add stuff

---
+++ webloop/update.lua
@@ -0,0 +1,12 @@
+local path = shell.dir() .. "/"
+
+local function loadFile(url, fileName)
+    local code = http.get(url).readAll()
+    local file = fs.open(path .. fileName, "w")
+    file.write(code)
+    file.close()
+end
+
+loadFile("https://raw.githubusercontent.com/Pebloop/pebox/main/webloop/start.lua", "start.lua")
+
+loadFile("https://raw.githubusercontent.com/Pebloop/pebox/main/webloop/example.lua", "example.lua")
