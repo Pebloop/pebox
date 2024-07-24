10648f922a129390bc67c16c1b251a35bb7c006e
pebloop <pebloop@outlook.com>
2024-07-24 21:28:45 +0200

    do stuff

--- webloop/style/center.lua
+++ webloop/style/center.lua
@@ -4,11 +4,10 @@ function Center.exec(data, size)
     local w, h = term.getSize() -- if not parent, use screen size as reference
 
     if data.parent then
-        print(textutils.serialise(data.parent))
         w = data.parent.data.width
         h = data.parent.data.height
+
     end
-    
     w = math.floor((w - size.width) / 2)
     data.cursorX = w
     return data
