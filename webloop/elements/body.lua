0f4c0e45c67af874f378921507389bdcc5593716
pebloop <pebloop@outlook.com>
2024-07-26 11:09:01 +0200

    do stuff

--- webloop/elements/body.lua
+++ webloop/elements/body.lua
@@ -25,10 +25,10 @@ local function computeWrappedSize(data, children)
     local ElementSize = require("data.elements_size")
     local wrappedSize = {width = 0, height = 0}
     for i, child in ipairs(children) do
-        if type(child) == "string" then
-            child = {type = "text", style = "", value = child}
-        end
-        local childSize = ElementSize[child.type](data, child.style, child.value)
+
+        local childData = data:child({x = data.x, y = data.y}, data.width, data.height)
+
+        local childSize = ElementSize[child.type](childData, child.style, child.value)
         wrappedSize.width = math.max(wrappedSize.width, childSize.width)
         wrappedSize.height = wrappedSize.height + childSize.height
     end
@@ -36,6 +36,12 @@ local function computeWrappedSize(data, children)
 end
 
 function Body.size(data, style, children)
+    for i, child in ipairs(children) do
+        if type(child) == "string" then
+            children[i] = {type = "text", style = "", value = child}
+        end
+    end
+    data = StyleManager.execute(data, style, {width=-1,height=-1})
     local childData = data:child({x = data.x, y = data.y}, data.width, data.height)
     childData.parent = data
     return computeWrappedSize(childData, children)
@@ -43,7 +49,6 @@ end
 
 function Body.exec(data, style, children)
     local ElementList = require("data.elements_list")
-    data = StyleManager.execute(data, style, {width=-1,height=-1})
 
     for i, child in ipairs(children) do
         if type(child) == "string" then
@@ -51,6 +56,7 @@ function Body.exec(data, style, children)
         end
     end
 
+    data = StyleManager.execute(data, style, {width=-1,height=-1})
     local childWrappedData = data:child({x = data.x, y = data.y}, data.width, data.height)
     childWrappedData.type = "div"
 
@@ -64,7 +70,7 @@ function Body.exec(data, style, children)
 
     term.setBackgroundColor(data.bg_color)
     for x = 1, data.width do
-        for y = 1, data.height do
+        for y = 1, data.height - 1 do
             term.setCursorPos(data.x + x - 1, data.y + y - 1)
             term.write(" ")
         end
@@ -78,9 +84,6 @@ function Body.exec(data, style, children)
         local childWrappedSize = computeWrappedSize(cwd, {child})
 
         local childData = data:child({x = data.x, y = data.y}, data.width, data.height)
-        if type(child) == "string" then
-            child = {type = "text", style = "", value = child}
-        end
         ElementList[child.type](childData, child.style, child.value)
         data.y = data.y + childWrappedSize.height
     end
--- webloop/example.lua
+++ webloop/example.lua
@@ -3,6 +3,6 @@ webloop(nil, body {
     div("w-10 bg-cyan") {
         "This is a div element",
         text("text-pink") {"this is another div element"},
-        text("middle text-green") { "This is a paragraph element"}
+        text("text-green") { "This is a paragraph element"}
     }
 })
