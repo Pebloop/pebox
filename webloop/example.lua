8523e92cbacd5db5bdaecb4580c91f54a5337127
pebloop <pebloop@outlook.com>
2024-07-26 14:31:22 +0200

    do stuff

--- webloop/example.lua
+++ webloop/example.lua
@@ -1,8 +1,13 @@
-webloop(nil, body {
+webloop(nil, body("bg-cyan w-screen") {
     text("middle") {"Welcome to Webloop!"},
-    div("w-10 bg-cyan") {
+    div("w-10 bg-purple") {
         "This is a div element",
         text("text-pink") {"this is another div element"},
         text("text-green") { "This is a paragraph element"}
+    },
+    div("bg-red middle") {
+        {"This is a div element"},
+        {"this is another div element"},
+        { "This is a paragraph element"}
     }
 })
