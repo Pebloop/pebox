93e29b40eca5ce38ba213fca74d2cbdb1e8b701e
pebloop <pebloop@outlook.com>
2024-07-31 05:59:46 +0200

    do stuff

--- blood/src/states/save_as_state.lua
+++ blood/src/states/save_as_state.lua
@@ -11,6 +11,18 @@ function SaveAsState.events(event, window, data)
             data.stateData.path = data.stateData.path .. event[2]
         end
         data.isDirty = true
+    elseif event[1] == 'key' then
+        if event[2] == keys.enter then
+            if data.stateData.path ~= "" then
+                data.currentFile = fs.open(data.stateData.path, 'w')
+                data.currentFile.write(data.currentFile.content)
+                data.currentFile.close()
+                data.state = nil
+            end
+        elseif event[2] == keys.backspace then
+            data.stateData.path = data.stateData.path:sub(1, #data.stateData.path - 1)
+            data.isDirty = true
+        end
     end
 end
 
