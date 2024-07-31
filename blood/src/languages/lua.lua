2d6dd60cf422f8448ad0f84a5dc1d2840aec5b5f
pebloop <pebloop@outlook.com>
2024-07-31 03:54:47 +0200

    do stuff

--- blood/src/languages/lua.lua
+++ blood/src/languages/lua.lua
@@ -7,12 +7,11 @@ function LuaLang.pretty(code)
 
     for line in string.gmatch(code, "[^\n]+") do
         for token in string.gmatch(line, "%S+") do
-            doc = pretty.concat(doc, pretty.text(token, colors.cyan), pretty.space)
+            doc = pretty.concat(doc, pretty.text(token, colours.blue), pretty.space)
         end
-        doc = pretty.concat(doc, pretty.space_line)
-        doc = pretty.group(doc)
+        doc = pretty.concat(doc, pretty.line)
     end
-    return doc
+    return pretty.render(doc)
 end
 
 return LuaLang
