bba42b1ba4eac8b6220c6e58229be74045c62e73
pebloop <pebloop@outlook.com>
2024-07-31 04:57:46 +0200

    do stuff

--- blood/src/languages/lua.lua
+++ blood/src/languages/lua.lua
@@ -10,27 +10,49 @@ local tokenColors = {
 function LuaLang.pretty(code, window)
     local doc = Pretty.doc()
     local buffer = ""
+    local state = "init"
 
     for c in code:gmatch(".") do
-        if c == " " then
-            Pretty.append(doc, Pretty.token(buffer, Colors.text))
-            buffer = ""
-            Pretty.append(doc, Pretty.space())
-        elseif c == "\n" then
-            Pretty.append(doc, Pretty.newline())
-        else
-            buffer = buffer .. c
-            if buffer == "--" then
+        if state == "init" then
+            if c == " " then
+                Pretty.append(doc, Pretty.token(buffer, Colors.text))
+                buffer = ""
+                Pretty.append(doc, Pretty.space())
+            elseif c == "\n" then
+                Pretty.append(doc, Pretty.newline())
+            else
+                buffer = buffer .. c
+                if buffer == "--" then
+                    state = "comment"
+                elseif buffer == "\"" then
+                    state = "string"
+                elseif tokenColors[buffer] then
+                    Pretty.append(doc, Pretty.token(buffer, tokenColors[buffer]))
+                    buffer = ""
+                end
+            end
+
+        elseif state == "comment" then
+            if c == "\n" then
                 Pretty.append(doc, Pretty.token(buffer, Colors.text4))
                 buffer = ""
-            elseif tokenColors[buffer] then
-                Pretty.append(doc, Pretty.token(buffer, tokenColors[buffer]))
+                state = "init"
+            else
+                buffer = buffer .. c
+            end
+        elseif state == "string" then
+            if c == "\"" then
+                buffer = buffer .. c
+                Pretty.append(doc, Pretty.token(buffer, Colors.text3))
                 buffer = ""
+                state = "init"
+            else
+                buffer = buffer .. c
             end
         end
     end
     Pretty.append(doc, Pretty.token(buffer, Colors.text))
-    
+
     Pretty.print(doc, window)
 end
 
